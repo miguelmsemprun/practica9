ef2f903a3e6ab64e8cd59978f38a19033e7ff341
miguel.msemprun <miguel.msemprun@alumnos.upm.es>
2016-05-26 16:56:31 +0200

    Tema 24: Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/author', function(req, res, next) {
 // Autoload de parámetros
 router.param('quizId', quizController.load);  // autoload :quizId
 router.param('userId', userController.load);  // autoload :userId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/session',    sessionController.new);     // formulario login
@@ -41,7 +42,8 @@ router.post('/quizzes', sessionController.loginRequired,    			   			quizControl
 router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, 			quizController.edit);
 router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired,      			quizController.update);
 router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired,   			quizController.destroy);
-router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired,  commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired,     commentController.create);
+router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired,  					commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired,     					commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, 	commentController.accept);
 
 module.exports = router;
