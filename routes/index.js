eb1cbb5fb5c98b3c4ff1dc944ae4d977491cd587
miguel.msemprun <miguel.msemprun@alumnos.upm.es>
2016-05-26 17:57:07 +0200

    Tema 25: Sin terminar

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,9 @@ router.get('/users',                    userController.index);   // listado usua
 router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
 router.get('/users/new',                userController.new);     // formulario sign un
 router.post('/users',                   userController.create);  // registrar usuario
-router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit);     // editar información de cuenta
-router.put('/users/:userId(\\d+)', sessionController.loginRequired, userController.update);   // actualizar información de cuenta
-router.delete('/users/:userId(\\d+)', sessionController.loginRequired, userController.destroy);  // borrar cuenta
+router.get('/users/:userId(\\d+)/edit', sessionController.loginRequired, userController.ownershipRequired, userController.edit);     // editar información de cuenta
+router.put('/users/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.update);   // actualizar información de cuenta
+router.delete('/users/:userId(\\d+)', sessionController.loginRequired, userController.ownershipRequired, userController.destroy);  // borrar cuenta
 
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
@@ -39,11 +39,13 @@ router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/new', sessionController.loginRequired, 			   			quizController.new);
 router.post('/quizzes', sessionController.loginRequired,    			   			quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit', sessionController.loginRequired, 			quizController.edit);
-router.put('/quizzes/:quizId(\\d+)', sessionController.loginRequired,      			quizController.update);
-router.delete('/quizzes/:quizId(\\d+)', sessionController.loginRequired,   			quizController.destroy);
+router.get('/quizzes/:quizId(\\d+)/edit', userController.ownershipRequired, sessionController.loginRequired, 			quizController.edit);
+router.put('/quizzes/:quizId(\\d+)', userController.ownershipRequired, sessionController.loginRequired,      			quizController.update);
+router.delete('/quizzes/:quizId(\\d+)', userController.ownershipRequired,sessionController.loginRequired,   			quizController.destroy);
+
+// Para los comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new', sessionController.loginRequired,  					commentController.new);
-router.post('/quizzes/:quizId(\\d+)/comments', sessionController.loginRequired,     					commentController.create);
-router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, 	commentController.accept);
+router.post('/quizzes/:quizId(\\d+)/comments',     														commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired, 	userController.ownershipRequired, commentController.accept);
 
 module.exports = router;
